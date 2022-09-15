bca83a7afbba5fcde7ef9da1019f7b5f98fd806b
Григорий Федорущенко <Gfedorushenko@mail.ru>
2022-09-15 06:39:06 +0300

    v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,13 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Main {
+
     public static void main(String[] args) {
+        String host = "netology.homework";
+        int port = 8888;
         String text ="Hello!";
         while (!text.equals("end") && text!=null) {
-            text = newConnect(text);
+            text = newConnect(text,host,port);
             switch (text) {
                 case "Let's talk":text="ok";break;
                 case "where are you from?":text="from Russia";break;
@@ -23,9 +26,7 @@ public class Main {
         }
     }
 
-    public static String newConnect(String text) {
-        String host = "netology.homework";
-        int port = 8888;
+    public static String newConnect(String text,String host,int port) {
         try (Socket clientSocket = new Socket(host, port);
              PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
              BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
